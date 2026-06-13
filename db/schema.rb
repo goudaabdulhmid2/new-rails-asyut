2403c3ca4979f7974acbf9e9c84dac26bbfcb7be
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2026-06-13 12:55:30 +0300

    feat: initialize database schema with articles, posts, and users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_12_124217) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_13_124600) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.date "DOB"
+    t.string "address", default: "", null: false
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "phone_number"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
